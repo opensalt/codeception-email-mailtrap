221dfe54c209aa5480c584eddc2910a25acbc7ea
Eric Martel <emartel@gmail.com>
2016-01-02 20:55:12 -0500

    Initial commit
    Not working yet

---
+++ README.md
@@ -0,0 +1,105 @@
+# Mailtrap Service Provider for the Codeception Email Testing Framework
+
+This Codeception Module implements the required methods to test emails using the [Codeception Email Testing Framework][CodeceptionEmailTestingFramework] with [Mailtrap]
+
+### Installation
+Through composer, require the package:
+```
+"require-dev": {
+        "ericmartel/codeception-email-mailtrap": "^1.0"
+    }
+```
+Then turn it on in your Codeception suite yaml file
+```
+class_name: FunctionalTester
+modules:
+    enabled:
+        - Mailtrap
+    config:
+        Mailtrap:
+            api_token: 'your token'
+            inbox_id: 'the inbox id you're using'
+```
+Additional parameters can be fed directly to the Guzzle connection using the `guzzleRequestOptions` variable.
+
+The variable `deleteEmailsAfterScenario` can be set to true to ensure that all emails are deleted at the end of each scenario, but it is turned off by default.
+### Added Methods
+This Module adds a few public methods for the user, such as:
+```
+deleteAllEmails()
+```
+Deletes all emails in MailHog
+```
+fetchEmails()
+```
+Fetches all email headers from MailHog, sorts them by timestamp and assigns them to the current and unread inboxes
+```
+accessInboxFor($address)
+```
+Filters emails to only keep those that are received by the provided address
+```
+openNextUnreadEmail()
+```
+Pops the most recent unread email and assigns it as the email to conduct tests on
+
+### Example Test
+Here is a simple scenario where we test the content of an email.  For a detailed list of all available test methods, please refer to the [Codeception Email Testing Framework][CodeceptionEmailTestingFramework].
+```
+<?php 
+$I = new FunctionalTester($scenario);
+$I->am('a member');
+$I->wantTo('request a reset password link');
+
+// First, remove all existing emails in the MailHog inbox
+$I->deleteAllEmails();
+
+// Implementation is up to the user, use this as an example
+$I->requestAPasswordResetLink();
+
+// Query MailHog and fetch all available emails
+$I->fetchEmails();
+
+// This is optional, but will filter the emails in case you're sending multiple emails or use the BCC field
+$I->accessInboxFor('testuser@example.com');
+
+// A new email should be available and it should be unread
+$I->haveEmails();
+$I->haveUnreadEmails();
+
+// Set the next unread email as the email to perform operations on
+$I->openNextUnreadEmail();
+
+// After opening the only available email, the unread inbox should be empty
+$I->dontHaveUnreadEmails();
+
+// Validate the content of the opened email, all of these operations are performed on the same email
+$I->seeInOpenedEmailSubject('Your Password Reset Link');
+$I->seeInOpenedEmailBody('Follow this link to reset your password');
+$I->seeInOpenedEmailRecipients('testuser@example.com');
+```
+
+### License
+Copyright (c) 2015 Eric Martel, http://github.com/ericmartel <emartel@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+   [MailHog]: http://mailtrap.io
+   [CodeceptionEmailTestingFramework]: https://github.com/ericmartel/codeception-email
+   
+
---
+++ src/mailtrap.php
@@ -0,0 +1,429 @@
+<?php
+
+/*
+ * This file is part of the Mailtrap service provider for the Codeception Email Testing Framework.
+ * (c) 2015 Eric Martel
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Codeception\Module;
+
+use Codeception\Module;
+
+class Mailtrap extends Module
+{
+  use \Codeception\Email\TestsEmails;
+
+  use \Codeception\Email\EmailServiceProvider;
+
+  /**
+   * HTTP Client to interact with Mailtrap
+   *
+   * @var \GuzzleHttp\Client
+   */
+  protected $mailtrap;
+
+  /**
+   * Raw email header data converted to JSON
+   *
+   * @var array
+   */
+  protected $fetchedEmails;
+
+  /**
+   * Currently selected set of email headers to work with
+   *
+   * @var array
+   */
+  protected $currentInbox;
+
+  /**
+   * Starts as the same data as the current inbox, but items are removed as they're used
+   *
+   * @var array
+   */
+  protected $unreadInbox;
+
+  /**
+   * Contains the currently open email on which test operations are conducted
+   *
+   * @var mixed
+   */
+  protected $openedEmail;
+
+  /**
+   * Codeception exposed variables
+   *
+   * @var array
+   */
+  protected $config = array('api_token', 'inbox_id', 'guzzleRequestOptions', 'deleteEmailsAfterScenario');
+
+  /**
+   * Codeception required variables
+   *
+   * @var array
+   */
+  protected $requiredFields = array('api_token', 'inbox_id');
+
+  public function _initialize()
+  {
+    $url = "https://mailtrap.io/"
+
+    $this->mailtrap = new \GuzzleHttp\Client(['base_uri' => $url, 'timeout' => 1.0]);
+
+    if (isset($this->config['guzzleRequestOptions'])) {
+        foreach ($this->config['guzzleRequestOptions'] as $option => $value) {
+            $this->mailtrap->setDefaultOption($option, $value);
+        }
+    }
+  }
+
+  /** 
+   * Method executed after each scenario
+   */
+  public function _after()
+  {
+    if(isset($this->config['deleteEmailsAfterScenario']) && $this->config['deleteEmailsAfterScenario'])
+    {
+      $this->deleteAllEmails();
+    }
+  }
+
+  protected function sendRequest($verb, $resource)
+  {
+    return $this->mailtrap->request($verb, $resource, ['headers' => ['Api-Token' => $this->config['api_token']]]);
+  }
+
+  /** 
+   * Delete All Emails
+   *
+   * Accessible from tests, deletes all emails 
+   */
+  public function deleteAllEmails()
+  {
+    try
+    {
+      $this->sendRequest('PATCH', "api/v1/inboxes/{$this->config['inbox_id']}/clean");
+    }
+    catch(Exception $e)
+    {
+      $this->fail('Exception: ' . $e->getMessage());
+    }
+  }
+
+  /** 
+   * Fetch Emails
+   *
+   * Accessible from tests, fetches all emails 
+   */
+  public function fetchEmails()
+  {
+    $this->fetchedEmails = array();
+
+    try
+    {
+      $response = $this->sendRequest('GET', "/api/v1/inboxes/{$this->config['inbox_id']}/messages");
+      $this->fetchedEmails = json_decode($response->getBody());
+    }
+    catch(Exception $e)
+    {
+      $this->fail('Exception: ' . $e->getMessage());
+    }
+
+    $this->sortEmails($this->fetchedEmails);
+
+    // by default, work on all emails
+    $this->setCurrentInbox($this->fetchedEmails);
+  }
+
+  /** 
+   * Access Inbox For
+   * 
+   * Filters emails to only keep those that are received by the provided address
+   *
+   * @param string $address Recipient address' inbox
+   */
+  public function accessInboxFor($address)
+  {
+    $inbox = array();
+    $addressPlusDelimiters = '<' . $address . '>';
+    foreach($this->fetchedEmails as $email)
+    {
+      if(!isset($email->Content->Headers->Bcc))
+      {
+        if(strpos($email->Content->Headers->To[0], $addressPlusDelimiters) || array_search($email->Content->Headers->Cc[0], $addressPlusDelimiters))
+        {
+          array_push($inbox, $email);
+        }
+      }
+      else if(strpos($email->Content->Headers->Bcc[0], $addressPlusDelimiters))
+      {
+        array_push($inbox, $email);
+      }
+    }
+    $this->setCurrentInbox($inbox);
+  }
+
+  /** 
+   * Open Next Unread Email
+   *
+   * Pops the most recent unread email and assigns it as the email to conduct tests on
+   */
+  public function openNextUnreadEmail()
+  {
+    $this->openedEmail = $this->getMostRecentUnreadEmail();
+  }
+
+  /**
+   * Get Opened Email
+   *
+   * Main method called by the tests, providing either the currently open email or the next unread one
+   *
+   * @param bool $fetchNextUnread Goes to the next Unread Email
+   * @return mixed Returns a JSON encoded Email
+   */
+  protected function getOpenedEmail($fetchNextUnread = FALSE)
+  {
+    if($fetchNextUnread || $this->openedEmail == NULL)
+    {
+      $this->openNextUnreadEmail();
+    }
+
+    return $this->openedEmail;
+  }
+
+  /**
+   * Get Most Recent Unread Email
+   * 
+   * Pops the most recent unread email, fails if the inbox is empty
+   * 
+   * @return mixed Returns a JSON encoded Email
+   */
+  protected function getMostRecentUnreadEmail()
+  {
+    if(empty($this->unreadInbox))
+    {
+      $this->fail('Unread Inbox is Empty');
+    }
+
+    $email = array_shift($this->unreadInbox);
+    return $this->getFullEmail($email->ID);
+  }
+
+  /**
+   * Get Full Email
+   * 
+   * Returns the full content of an email
+   *
+   * @param string $id ID from the header
+   * @return mixed Returns a JSON encoded Email
+   */
+  protected function getFullEmail($id)
+  {
+    try
+    {
+      $response = $this->sendRequest('GET', "/api/v1/inboxes/{$this->config['inbox_id']}/messages/{$id}");
+    }
+    catch(Exception $e)
+    {
+      $this->fail('Exception: ' . $e->getMessage());
+    }
+    $fullEmail = json_decode($response->getBody());
+    return $fullEmail;
+  }
+
+  /**
+   * Get Email Subject
+   *
+   * Returns the subject of an email
+   *
+   * @param mixed $email Email
+   * @return string Subject
+   */
+  protected function getEmailSubject($email)
+  {
+    return $email->Content->Headers->Subject[0];
+  }
+
+  /**
+   * Get Email Body
+   *
+   * Returns the body of an email
+   *
+   * @param mixed $email Email
+   * @return string Body
+   */
+  protected function getEmailBody($email)
+  {
+    return $email->Content->Body;
+  }
+
+  /**
+   * Get Email To
+   *
+   * Returns the string containing the persons included in the To field
+   *
+   * @param mixed $email Email
+   * @return string To
+   */
+  protected function getEmailTo($email)
+  {
+    return $email->Content->Headers->To[0];
+  }
+
+  /**
+   * Get Email CC
+   *
+   * Returns the string containing the persons included in the CC field
+   *
+   * @param mixed $email Email
+   * @return string CC
+   */
+  protected function getEmailCC($email)
+  {
+    return $email->Content->Headers->Cc[0];
+  }
+
+  /**
+   * Get Email BCC
+   *
+   * Returns the string containing the persons included in the BCC field
+   *
+   * @param mixed $email Email
+   * @return string BCC
+   */
+  protected function getEmailBCC($email)
+  {
+    if(isset($email->Content->Headers->Bcc))
+    {
+      return $email->Content->Headers->Bcc[0];
+    }
+    return "";
+  }
+
+  /**
+   * Get Email Recipients
+   *
+   * Returns the string containing all of the recipients, such as To, CC and if provided BCC
+   *
+   * @param mixed $email Email
+   * @return string Recipients
+   */
+  protected function getEmailRecipients($email)
+  {
+    $recipients = $email->Content->Headers->To[0] . ' ' .
+                  $email->Content->Headers->Cc[0];
+    if(isset($email->Content->Headers->Bcc))
+    {
+      $recipients .= ' ' . $email->Content->Headers->Bcc[0];  
+    }
+
+    return $recipients;
+  }
+
+  /**
+   * Get Email Sender
+   *
+   * Returns the string containing the sender of the email
+   *
+   * @param mixed $email Email
+   * @return string Sender
+   */
+  protected function getEmailSender($email)
+  {
+    return $email->Content->Headers->From[0];
+  }
+
+  /**
+   * Get Email Reply To
+   *
+   * Returns the string containing the address to reply to
+   *
+   * @param mixed $email Email
+   * @return string ReplyTo
+   */
+  protected function getEmailReplyTo($email)
+  {
+    return $email->Content->Headers->{'Reply-To'}[0];
+  }
+
+  /**
+   * Get Email Priority
+   * 
+   * Returns the priority of the email
+   * 
+   * @param mixed $email Email
+   * @return string Priority
+   */
+  protected function getEmailPriority($email)
+  {
+    return $email->Content->Headers->{'X-Priority'}[0];
+  }
+
+  /**
+   * Set Current Inbox
+   *
+   * Sets the current inbox to work on, also create a copy of it to handle unread emails
+   *
+   * @param array $inbox Inbox
+   */
+  protected function setCurrentInbox($inbox)
+  {
+    $this->currentInbox = $inbox;
+    $this->unreadInbox = $inbox;
+  }
+
+  /**
+   * Get Current Inbox
+   *
+   * Returns the complete current inbox
+   *
+   * @return array Current Inbox
+   */
+  protected function getCurrentInbox()
+  {
+    return $this->currentInbox;
+  }
+
+  /**
+   * Get Unread Inbox
+   *
+   * Returns the inbox containing unread emails
+   *
+   * @return array Unread Inbox
+   */
+  protected function getUnreadInbox()
+  {
+    return $this->unreadInbox;
+  }
+
+  /**
+   * Sort Emails
+   *
+   * Sorts the inbox based on the timestamp
+   *
+   * @param array $inbox Inbox to sort
+   */
+  protected function sortEmails($inbox)
+  {
+    usort($inbox, array($this, 'sortEmailsByCreationDatePredicate'));
+  }
+
+  /**
+   * Get Email To
+   *
+   * Returns the string containing the persons included in the To field
+   *
+   * @param mixed $emailA Email
+   * @param mixed $emailB Email
+   * @return int Which email should go first
+   */
+  static function sortEmailsByCreationDatePredicate($emailA, $emailB) 
+  {
+    $sortKeyA = $emailA->Content->Headers->Date;
+    $sortKeyB = $emailB->Content->Headers->Date;
+    return ($sortKeyA > $sortKeyB) ? -1 : 1;
+  }
+}
